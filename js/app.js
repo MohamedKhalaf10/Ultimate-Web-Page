bacd811bdb2f6ac8299257de5b80ff1c88bf738d
Mohamed Khalaf <mokhalaf10@gmail.com>
2022-05-27 21:00:49 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const otherLinks = document.querySelector("#other");
 const megaMenu = document.querySelector(".mega-menu");
 
+const ourSkills = document.querySelector("#our-skills");
+const progs = [...document.querySelectorAll(".progress span")];
+
 otherLinks.addEventListener("click", (e) => {
   e.preventDefault();
   if (megaMenu.style.transform === "translateY(100%)") {
@@ -11,3 +14,11 @@ otherLinks.addEventListener("click", (e) => {
     megaMenu.style.opacity = "0";
   }
 });
+
+window.onscroll = () => {
+  if (window.scrollY >= ourSkills.offsetTop - 300) {
+    for (let prog of progs) {
+      prog.style.width = prog.dataset.width;
+    }
+  }
+};
