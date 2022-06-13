001b16d2c3bb1631fd75065355fca91641f6a0b8
Mohamed Khalaf <mokhalaf10@gmail.com>
2022-06-13 16:12:57 +0200

    General Modifications

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+/* Start Go Up */
+
+let goUp = document.querySelector(".go-up");
+
+function goUpFunc() {
+  if (this.scrollY === 0) {
+    goUp.style.right = "-100px";
+  } else {
+    goUp.style.right = "10px";
+  }
+}
+// Scroll To Top On Click
+goUp.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+});
+
+/* End Go UP */
 /* Start Landing */
 let otherLinks = document.querySelector("#other");
 let megaMenu = document.querySelector(".mega-menu");
@@ -83,10 +100,65 @@ let statsFunc = () => {
   }
 };
 /* End Stats */
+/* Start Top Videos */
 
+// Get Videos List
+let videos = [...document.querySelectorAll("#top-videos ul li")];
+// Get Video Img
+let img = document.querySelector(".video");
+
+// Get Random Icon
+let random = document.querySelector(".random");
+
+// Declare Click Event Function
+function topVideos(element) {
+  // Change Img Src
+  img.innerHTML = `<img src="images/videos-0${element}.jpg"">
+      <div class="description">Everything About The Virtual Hosts</div> `;
+  // Remove active-video Class From The Active Video
+  document.querySelector(".active-video").classList.remove("active-video");
+}
+
+// Loop Over The Videos List
+for (let vid of videos) {
+  // Get data-video Value
+  let dataVideo = vid.dataset.video;
+
+  // Add Event Listener To Each Video On Click
+  vid.addEventListener("click", () => {
+    // Execute topVideos Function
+    topVideos(dataVideo);
+    // Add active-video Class To The New Clicked Video
+    vid.classList.add("active-video");
+  });
+}
+// Add Event Listener To Random Icon On Click
+random.addEventListener("click", () => {
+  // Get Random Number From 1 To 7
+  let randomNum = Math.floor(Math.random() * 7) + 1;
+  if (
+    document.getElementById(`${randomNum}`) !=
+    document.querySelector(".active-video")
+  ) {
+    // Execute topVideos Function
+    topVideos(randomNum);
+    // Add active-video Class To The New Clicked Video
+    document.getElementById(`${randomNum}`).classList.add("active-video");
+  } else {
+    // Get New Random Number
+    randomNum = Math.floor(Math.random() * 7) + 1;
+    // Execute topVideos Function
+    topVideos(randomNum);
+    // Add active-video Class To The New Clicked Video
+    document.getElementById(`${randomNum}`).classList.add("active-video");
+  }
+});
+
+/* End Top Videos */
 /* Start On Scroll Functions */
 window.onscroll = () => {
   skillsFunc();
   statsFunc();
+  goUpFunc();
 };
 /* End On Scroll Functions */
